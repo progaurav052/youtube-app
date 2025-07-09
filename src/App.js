c18a02160fb7bfd6b7e926d7f49666d3c1ffb9fd
gaurav pai <gpprojects511@gmail.com>
2025-07-09 19:46:10 +0530

    Header Design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-
-
+import "./index.css"
+import Header from "./components/Header";
+import Body from "./components/Body";
 function App() {
   return (
-    <h1>Namaste youtube</h1>
+     <div>
+      <Header/>
+      <Body/>
+     </div>
   );
 }
+
+/*{
+ -Head 
+ -Body
+   -sidebar
+     -menuItems
+   -MainContainer
+     -Buttonlist
+     -videoContainer
+       -videocard 
+
+
+}*/
 
 export default App;
